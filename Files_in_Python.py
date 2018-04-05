0cb02a45b229215d43452c2f3f29642b0aac1833
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-04-05 21:44:00 +0300

    Add files via upload

---
+++ Files_in_Python.py
@@ -0,0 +1,59 @@
+# Working with files
+inf = open('test.txt', 'r')  # Opening the file
+s1 = inf.readline()
+s2 = inf.readline()
+inf.close()  # Closing the file
+
+print(s1)
+print(s2)
+
+# Another way ho to open and close the file - it is recommended to use this way
+with open('test.txt') as inf:
+    s1 = inf.readline()
+    s2 = inf.readline()
+
+# Here file is alredy closed
+print(s1)
+print(s2)
+
+# Useful function to eliminate simboles at the beginning and at the and while reading line from the file
+with open('test.txt') as inf:
+    s1 = inf.readline().strip()  # Now at the and it will not add the '\n'
+    s2 = inf.readline().strip()
+print(s1)
+print(s2)
+
+# Another ueful function to specify the way to the file in standard way for all OS
+print()
+import os  # Module we need in this case
+p = os.path.join('.', 'dirname', 'filename.txt')  # First is folder, then subfolder and then filename
+# The amount of folders can be any, at the end we type the name of the file
+print(p)
+print()
+# For the Linux System it will show: './dirname/filename.txt'
+# For the Windows System it will show: '.\dirname\filename.txt'
+
+# Using loops
+with open('test.txt') as inf:
+    for line in inf:
+        line = line.strip()
+        print(line)
+
+# Writing in the file
+ouf = open('test1.txt', 'w')
+ouf.write('Test text\n')  # Here we need to add '\n' to move to the next line
+ouf.write(str(101))  # Here we need to use 'str()' to convert number to string
+ouf.close()
+
+# Another way to write in the file
+with open('test1.txt', 'w') as ouf:
+    ouf.write('Test text\n')
+    ouf.write('str(202)')
+
+# Checking if it is written in the file
+# Pay attention that each new time we open file to 'w' it will delete everything it was before inside
+print()
+with open('test1.txt') as inf:
+    for line in inf:
+        line = line.strip()
+        print(line)
